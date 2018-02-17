1032f840d6e01470ec524fd5d52d5a5449f7c837
Tomoaki Mikami <tmikami@gmail.com>
2018-02-17 16:24:52 +0900

    Initial Commit

---
+++ src/main/java/com/example/demo/ServeExternalImageApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeExternalImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeExternalImageApplication.class, args);
+	}
+}
